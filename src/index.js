251e4c5574afbd83348cf6c06c992f26c8c26248
Zoran Obradović <zockyo@gmail.com>
2019-10-05 21:23:29 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { WebShellApp } from "@websh/web-shell-app";
+
+const textarea = document.getElementById('textarea');
+
+WebShellApp.manifest({
+  name:"Text Area",
+  api: {
+    test:{},
+    file: {
+      formats: {
+        text: {
+          type:"text/plain",
+          open: true,
+          save: true,
+          new: true
+        }
+      }
+    }
+  }
+})
+.command('app-close', () => { WebShellApp.throw("app-close-wait") })
+.command('file-open', function({format,extension,content}){
+  textarea.value = content;
+  return "OK";
+})
+.command('file-save', function({format,extension}){
+  return {content:textarea.value,type:"text/plain"};
+})
+.command('file-new', function({format,extension}){
+  textarea.value = "";
+  return "OK"
+});
